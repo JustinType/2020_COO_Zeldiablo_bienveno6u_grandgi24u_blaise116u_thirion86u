fa060efb3548ed7e8ca9ce1c53bb96f65be19edf
Valentin Thirion <valentin.thirion6@etu.univ-lorraine.fr>
2020-09-02 09:13:22 +0200

    Je change la merde d'aurélien

--- projet_zeldiablo/src/test/PlayerTest.java
+++ projet_zeldiablo/src/test/PlayerTest.java
@@ -1,11 +1,10 @@
 package test;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
-import Player;
+import application.Player;
 
 class PlayerTest {
 
