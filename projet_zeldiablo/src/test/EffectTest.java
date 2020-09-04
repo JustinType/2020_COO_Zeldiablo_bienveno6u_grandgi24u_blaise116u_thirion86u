b404bb6a7b66e2762777a3c5099674aa2b7d71b2
AurelienBls <aurelien.bls88@gmail.com>
2020-09-04 08:51:15 +0200

    ajout test effet 2

---
+++ projet_zeldiablo/src/test/EffectTest.java
@@ -0,0 +1,35 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import application.Effect;
+import application.Jeu;
+import application.Player;
+
+public class EffectTest {
+	Jeu j = new Jeu();
+	Player p1 = new Player(400,320,10);
+	
+	
+	@Test
+	public void test() {
+		p1.setEffect(new Effect(5,1));
+		p1.direction("up");
+		p1.doEffect();
+		assertEquals("La vitesse devrais etre a 1",1,p1.getvitesse());
+		p1.doEffect();
+		assertEquals("La vitesse devrais etre a 1",1,p1.getvitesse());
+		p1.doEffect();
+		assertEquals("La vitesse devrais etre a 1",1,p1.getvitesse());
+		p1.doEffect();
+		assertEquals("La vitesse devrais etre a 1",1,p1.getvitesse());
+		p1.doEffect();
+		assertEquals("La vitesse devrais etre a 1",3,p1.getvitesse());
+		
+		
+		
+	}
+
+}
